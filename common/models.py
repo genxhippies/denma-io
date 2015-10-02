6d0cc022182d23607b8ebd1d5b4f61759e444ef8
Choi Kirin <iizs@naver.com>
2015-10-02 14:17:43 +0000

    Add note field to Episode model

--- common/models.py
+++ common/models.py
@@ -5,6 +5,7 @@ class Episode(models.Model):
     num = models.SmallIntegerField()
     subtitle = models.CharField(max_length=1024)
     publish_date = models.DateTimeField()
+    note = models.TextField(default='', blank=True)
 
     def __unicode__(self):
         return self.subtitle
