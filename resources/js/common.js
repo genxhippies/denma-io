50d9eb4ff852f7149fa7fcabb77d8e82d4b0ca63
Jeonghwan Jang <nundefined@nundefined.com>
2015-12-04 01:24:42 +0900

    made layer popup pretty.
    - added handlebars.js template

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,31 +1,48 @@
 var $layer;
+var layerTemplate;
 
 function init() {
     $layer = $('._layer').css({
 		position: 'absolute',
 		width: 300,
-		border: '1px solid #666',
-		backgroundColor: '#fff',
 		opacity: 0.9,
 		zIndex: 1000
 	});
+
+	$layer.on('click', function (e) {
+		e.preventDefault();
+
+		if ($(e.target).closest('.__close').length > 0) {
+			$layer.hide();
+		}
+	});
+
+	layerTemplate = Handlebars.compile($('._layerTemplate').html());
 }
 
 function getHtml(data) {
-	var subTitle = '<h3>' + data.subtitle + ' </h3>';
-	var pubDate = '<div>게시일: ' + data.publish_date + '</div>';
-	var characters = '<div>등장 인물: ' + data.characters + '</div>';
-	var url = '<div><a href="' + data.url + '" target="_new">보기</a></div>';
-
-	return '<div>' + subTitle + pubDate + characters + url + '</div>';
+	return layerTemplate({
+		heading: data.subtitle,
+		pubDate: data.publish_date,
+		characters: data.characters,
+		url: data.url
+	});
 }
 
 function drawLayer(result) {
-	$layer.html(getHtml(result));
+	if ($layer.is(':visible')) {
+		$layer.html(getHtml(result));
+	}
 }
 
 function showLoading(offset) {
-	$layer.html('<div>loading...</div>').css({
+	var $output = $(layerTemplate({
+		heading: '정보를 불러오고 있습니다.'
+	}));
+
+	$output.find('.__body').hide();
+
+	$layer.html($output.html()).css({
 		left: offset.left - 60,
 		top: offset.top + 18 + 4,
 	}).show();
